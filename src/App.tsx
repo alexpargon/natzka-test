ec5bb606d630a12b5c53f97d64644e1431644deb
Alejandro Parcet Gonzalez <alexpargon@gmail.com>
2024-12-19 11:23:32 +0100

    feat: added all basic features such as testing, tailwind, basic UI, and more
    Signed-off-by: Alejandro Parcet Gonzalez <alexpargon@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen bg-gray-100">
+      <div className="hidden md:flex flex-col w-64">
+        <div className="flex items-center justify-center h-16 bg-[#0f1649] border-b border-gray-200">
+          <span className="text-white font-bold uppercase">Natzka</span>
+        </div>
+        <div className="flex flex-col flex-1 overflow-y-auto">
+          <nav className="flex-1 px-2 py-4 bg-[#2538bb]">
+            <span className="text-white font-bold">Yet to be filled</span>
+          </nav>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex flex-col flex-1 overflow-y-auto">
+        <div className="flex items-center justify-between h-16 bg-[#0f1649] border-b border-gray-200">
+          <span className="text-white font-bold">/Path/to/success</span>
+        </div>
+        <div className="p-4">
+          <h1 className="text-2xl font-bold">representing /Path/to/success</h1>
+          <br />
+          <p className="mt-2 text-gray-600 whitespace-pre-wrap">- root </p>
+          <p className="mt-2 text-gray-600 whitespace-pre-wrap">
+            {"    "}- test
+          </p>
+          <p className="mt-2 text-gray-600 whitespace-pre-wrap">
+            {"    "}- something{" "}
+          </p>
+          <p className="mt-2 text-gray-600 whitespace-pre-wrap">
+            {"    "}
+            {"    "}- somewhere{" "}
+          </p>
+          <p className="mt-2 text-gray-600 whitespace-pre-wrap">
+            {"    "}- dev{" "}
+          </p>
+          <p className="mt-2 text-gray-600 whitespace-pre-wrap">
+            {"    "}
+            {"    "}- null
+          </p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
